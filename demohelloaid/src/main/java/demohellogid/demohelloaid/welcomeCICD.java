33aba1132d8dd9b78eddcc1760acc3d729e5644c
M ARUNKUMAR <mkarun1122@gmail.com>
2025-11-27 09:06:21 +0530

    first cicd project

---
+++ demohelloaid/src/main/java/demohellogid/demohelloaid/welcomeCICD.java
@@ -0,0 +1,12 @@
+package demohellogid.demohelloaid;
+
+public class welcomeCICD {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome CICD .....!");
+
+	}
+
+}
